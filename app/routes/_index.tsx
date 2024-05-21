5c5f703d6bda118e53a87ade7a83d77932cc9969
arlindrexhepi <axr3649@g.rit.edu>
2024-05-22 01:04:38 +0200

    Added material ui library and added custom theme with ThemeProvider and DarkModeProvider

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,35 +1,19 @@
-import type { MetaFunction } from '@remix-run/node'
-import { Link } from '@remix-run/react'
+import type { MetaFunction } from '@remix-run/node';
+import { Button, Typography } from '@mui/material';
+// import { Link } from '@remix-run/react';
 
 export const meta: MetaFunction = () => {
   return [
     { title: 'New Remix SPA' },
     { name: 'description', content: 'Welcome to Remix (SPA Mode)!' },
-  ]
-}
+  ];
+};
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1>Welcome to Remix (SPA Mode)</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/guides/spa-mode"
-            rel="noreferrer">
-            SPA Mode Guide
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-        <li>
-          <Link to="/montecarlo">Monte Carlo</Link>
-        </li>
-      </ul>
+    <div>
+      <Typography>Hello Remix</Typography>
+      <Button variant="contained">Primary Button</Button>
     </div>
-  )
+  );
 }
