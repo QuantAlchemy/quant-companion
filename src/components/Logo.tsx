9cd0ce3044e6c4fb6516a0449bd8bbb10138d330
Ralph Smith <ralph.github@fastmail.com>
2024-10-24 11:41:40 -0600

    fix: use a link around the logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,16 +5,20 @@ import type { Component } from 'solid-js'
 
 export const Logo: Component = () => {
   return (
-    <div class="flex items-center gap-3">
+    <a
+      class="flex items-center gap-3"
+      href="https://www.quantalchemy.io/"
+      target="_blank"
+    >
       <ImageRoot>
         <Image
           src={Icon}
           alt="Quant Companion"
         />
-        <ImageFallback>HN</ImageFallback>
+        <ImageFallback>QC</ImageFallback>
       </ImageRoot>
       <h1 class="font-normal text-2xl">Quant Companion</h1>
-    </div>
+    </a>
   )
 }
 
