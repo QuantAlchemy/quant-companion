eb5d129a0167faf1e260d4b036157bb277b835c1
Ralph Smith <ralph.github@fastmail.com>
2025-04-03 14:07:35 -0600

    chore: add vercel analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import { render } from 'solid-js/web'
+import { inject } from '@vercel/analytics'
 import App from './App'
 import './index.css'
+
+inject()
 
 render(() => <App />, document.getElementById('app')!)
